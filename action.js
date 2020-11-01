d310ff32493f122b88aeb5995e75acbbc9b5e1d0
liyingyu <mcps3621@gmail.com>
2020-11-01 20:28:47 +0800

    add helpers.js

--- action.js
+++ action.js
@@ -1,18 +1,3 @@
-class Car {
-    constructor(wheels) {
-        this.wheels = wheels;
-    }
-
-    drive(direction) {
-        return true;
-    }
-}
-
-function foo(params) {
-    // ...
-    return true;
-}
-
 let maserati = new Car(4);
 maserati.wheels = 2;
 maserati.drive('left');
---
+++ helpers.js
@@ -0,0 +1,14 @@
+class Car {
+    constructor(wheels) {
+        this.wheels = wheels;
+    }
+
+    drive(direction) {
+        return true;
+    }
+}
+
+function foo(params) {
+    // ...
+    return true;
+}
